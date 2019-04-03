076994989e56feec0ac8d4356fd29a62a714e05b
jsotteccanic <jsotteccanic@gmail.com>
2019-04-03 00:07:06 -0500

    Funciono! origin solo desde el hosting - seguridad

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,11 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, res) => {
+    // resp.addHeader("Access-Control-Allow-Origin", "https://inventario-app-aa28e.firebaseapp.com/");
+    res.header('Access-Control-Allow-Origin','https://inventario-app-aa28e.firebaseapp.com');
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+    res.send("Hello from Firebase!");
+});
